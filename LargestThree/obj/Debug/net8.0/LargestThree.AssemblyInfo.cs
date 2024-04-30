3ae1de6466f317ba97c17d00be2ec0dbdca871b7
Itsjustme27 <reachprayush@gmail.com>
2024-04-30 07:53:33 +0545

    factors

--- LargestThree/obj/Debug/net8.0/LargestThree.AssemblyInfo.cs
+++ LargestThree/obj/Debug/net8.0/LargestThree.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LargestThree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a06be52c4d963d4d9a487e80355f15a930bac39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72c2c7fa17afcc61e08635529bd2e3d39b9b5a3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LargestThree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LargestThree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ factors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace App
+{
+    class App{
+        public static void Main(string[] args){
+            int num;
+            Console.WriteLine("Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= num; i++){
+                if(num%i == 0){
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
